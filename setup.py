63fe319cf1401161ad4d38a45fdf44ff10a77ab4
alejandro <alejandroalfonso1994@gmail.com>
2024-07-14 17:32:15 -0400

    adjust description from readme

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 with open('arikedb/__init__.py', 'r') as f:
     version = list(
         filter(lambda line: "__version__ =" in line, f.readlines())
@@ -10,12 +13,7 @@ setup(
     name="arikedb",
     version=version,
     description="Arikedb Python Client Library",
-    long_description="Welcome to the ArikeDB Rust library! "
-                     "This library provides an interface to interact "
-                     "with the Arike Database, an advanced real-time "
-                     "database solution. This documentation "
-                     "will guide you through the process of setting "
-                     "up and using the library effectively.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Alejandro Alfonso",
     author_email="alejandroalfonso1994@gmail.com",
