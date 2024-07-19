d64192af119cce3a5a024cc15e6461821e5b0fb7
alejandro <alejandroalfonso1994@gmail.com>
2024-07-18 22:46:49 -0400

    add publisher script

--- publisher.sh
+++ publisher.sh
@@ -3,3 +3,9 @@
 set -e
 
 python -m grpc_tools.protoc -I=proto --python_out=. --grpc_python_out=. proto/arikedbpbuff.proto
+
+rm -rf dist build
+
+python3 setup.py sdist bdist_wheel
+
+twine upload dist/*
